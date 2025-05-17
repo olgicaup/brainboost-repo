a9d072349df88afeb51308fa4bc4cde23e8d9a3b
da4e123 <dalibor.blazhevski@students.finki.ukim.mk>
2025-05-17 20:46:30 +0200

    Updated database

--- analyzer_app/models.py
+++ analyzer_app/models.py
@@ -9,3 +9,13 @@ class FavoriteDocument(models.Model):
 
     def __str__(self):
         return self.file_name
+
+
+class File(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    filename = models.CharField(max_length=255)
+    filepath = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.filename
